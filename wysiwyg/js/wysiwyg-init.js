d29b05a938c4304e6584a27d9f04a237c0b7bb8e
lcarcone <carcone@w3.org>
2015-12-02 15:03:14 +0100

    wysywyg experimentation

---
+++ wysiwyg/js/wysiwyg-init.js
@@ -0,0 +1,181 @@
+$(document).ready(function() {
+    // Featured editor
+    $('#xq1\\:comment').each( function(index, element)
+    {
+	//alert('index ' + index);
+        $(element).wysiwyg({
+            classes: 'some-more-classes',
+            // 'selection'|'top'|'top-selection'|'bottom'|'bottom-selection'
+            toolbar: 'top',
+            buttons: {
+                insertlink: {
+                    title: 'Insert link',
+                    image: '<img src="../wysiwyg/icons/link.png" alt="link" />',
+                },
+                // Header plugin
+                header: {
+                    title: 'Header',
+                    // image: '\uf1dc', 
+                    // image: '<img src="path/to/image.png" width="16" height="16" alt="" />',
+                    image: '<img src="../wysiwyg/icons/header.png" alt="Header" />',
+                    popup: function( $popup, $button ) {
+                            var list_headers = {
+                                    // Name : Font
+                                    // 'Header 1' : '<h1>',
+                                    'Header 2' : '<h2>',
+                                    'Header 3' : '<h3>',
+                                    'Header 4' : '<h4>',
+                                    'Header 5' : '<h5>',
+                                    'Header 6' : '<h6>',
+                                    'Code'     : '<pre>'
+                                };
+                            var $list = $('<div/>').addClass('wysiwyg-plugin-list')
+                                                   .attr('unselectable','on');
+                            $.each( list_headers, function( name, format ) {
+                                var $link = $('<a/>').attr('href','#')
+                                                     .css( 'font-family', format )
+                                                     .html( name )
+                                                     .click(function(event) {
+                                                        $(element).wysiwyg('shell').format(format).closePopup();
+                                                        // prevent link-href-#
+                                                        event.stopPropagation();
+                                                        event.preventDefault();
+                                                        return false;
+                                                    });
+                                $list.append( $link );
+                            });
+                            $popup.append( $list );
+                           }
+                    //showstatic: true,    // wanted on the toolbar
+                    //showselection: false    // wanted on selection
+                },
+                bold: {
+                    title: 'Bold (Ctrl+B)',
+                    image: '<img src="../wysiwyg/icons/bold.png" alt="bold" />',
+                    hotkey: 'b'
+                },
+                italic: {
+                    title: 'Italic (Ctrl+I)',
+                    image: '<img src="../wysiwyg/icons/italic.png" alt="italic" />',
+                    hotkey: 'i'
+                },
+                underline: {
+                    title: 'Underline (Ctrl+U)',
+                    image: '<img src="../wysiwyg/icons/underline.png" alt="underline" />',
+                    hotkey: 'u'
+                },
+                strikethrough: {
+                    title: 'Strikethrough (Ctrl+S)',
+                    image: '<img src="../wysiwyg/icons/strike.png" alt="Strikethrough" />',
+                    hotkey: 's'
+                },
+                forecolor: index == 0 ? false : {
+                    title: 'Text color',
+                    image: '\uf1fc' // <img src="path/to/image.png" width="16" height="16" alt="" />
+                },
+                highlight: index == 0 ? false :{
+                    title: 'Background color',
+                    image: '\uf043' // <img src="path/to/image.png" width="16" height="16" alt="" />
+                },
+                alignleft: index == 0 ? false : {
+                    title: 'Left',
+                    image: '\uf036', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                aligncenter: index == 0 ? false : {
+                    title: 'Center',
+                    image: '\uf037', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                alignright: index == 0 ? false : {
+                    title: 'Right',
+                    image: '\uf038', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                alignjustify: index == 0 ? false : {
+                    title: 'Justify',
+                    image: '\uf039', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                subscript: index == 0 ? false : {
+                    title: 'Subscript',
+                    image: '\uf12c', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: true    // wanted on selection
+                },
+                superscript: index == 0 ? false : {
+                    title: 'Superscript',
+                    image: '\uf12b', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: true    // wanted on selection
+                },
+                indent: index == 0 ? false : {
+                    title: 'Indent',
+                    image: '\uf03c', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                outdent: index == 0 ? false : {
+                    title: 'Outdent',
+                    image: '\uf03b', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                orderedList: {
+                    title: 'Ordered list',
+                    image: '<img src="../wysiwyg/icons/ol.png" alt="ordered list" />',
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                unorderedList: {
+                    title: 'Unordered list',
+                    image: '<img src="../wysiwyg/icons/ul.png" alt="unordered list" />',
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                removeformat: {
+                    title: 'Remove format',
+                    image: '<img src="../wysiwyg/icons/erase.png" alt="remove format" />',
+                }
+            },
+            // Submit-Button
+            submit: {
+                title: 'Submit',
+                image: '<img src="../wysiwyg/icons/ok.png" alt="submit" />' // <img src="path/to/image.png" width="16" height="16" alt="" />
+            },
+            // Other properties
+            selectImage: 'Click or drop image',
+            placeholderUrl: 'www.example.com',
+            placeholderEmbed: '<embed/>',
+            maxImageSize: [600,200],
+            onKeyDown: function( key, character, shiftKey, altKey, ctrlKey, metaKey ) {
+                            // E.g.: submit form on enter-key:
+                            //if( (key == 10 || key == 13) && !shiftKey && !altKey && !ctrlKey && !metaKey ) {
+                            //    submit_form();
+                            //    return false; // swallow enter
+                            //}
+                        },
+            onKeyPress: function( key, character, shiftKey, altKey, ctrlKey, metaKey ) {
+                        },
+            onKeyUp: function( key, character, shiftKey, altKey, ctrlKey, metaKey ) {
+                        }
+        })
+        .change(function() {
+            if( typeof console != 'undefined' )
+                ;//console.log( 'change' );
+        })
+        .focus(function() {
+            if( typeof console != 'undefined' )
+                ;//console.log( 'focus' );
+        })
+        .blur(function() {
+            if( typeof console != 'undefined' )
+                ;//console.log( 'blur' );
+        });
+    });
+
+});
